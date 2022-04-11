dff40cd7b4600ef808a5291cc9dfc31b7a86aa25
Huong Nguyen <nguyenhuonghh2707@gmail.com>
2022-04-11 21:07:25 +0700

    commit

---
+++ FOOD/app/utils/Format.js
@@ -0,0 +1,16 @@
+
+export const formatNumber = number => {
+  try {
+    const numberFormat = Math.round(parseFloat(number))
+      .toString()
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
+    return numberFormat
+  } catch (error) {
+    return 0
+  }
+}
+
+export const formatPrice = value => {
+  // return value
+  return Intl.NumberFormat().format(value)
+}
